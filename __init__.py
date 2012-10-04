9306eab4ed14cf8cbdfaaf7698928c367f02b1b1
ricin <github@ricin.oib.com>
2012-10-04 09:10:00 -0700

    oops

--- __init__.py
+++ __init__.py
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-###
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2008,2012 Kevin Funk
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#   * Redistributions of source code must retain the above copyright notice,
-#     this list of conditions, and the following disclaimer.
-#   * Redistributions in binary form must reproduce the above copyright notice,
-#     this list of conditions, and the following disclaimer in the
-#     documentation and/or other materials provided with the distribution.
-#   * Neither the name of the author of this software nor the name of
-#     contributors to this software may be used to endorse or promote products
-#     derived from this software without specific prior written consent.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
-###
-
-=======
->>>>>>> 27385ac875470495fb852184f052e5004972b2d0
 """
 Shows some information about a trakt.tv account (see: www.trakt.tv)
 """
@@ -49,11 +14,7 @@ __author__ = supybot.Author("", "", "")
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {
-<<<<<<< HEAD
-            supybot.Author("", "", ""): ["profile"]
-=======
             supybot.Author("Raymond Chandler", "intralanman", ""): ["profile"]
->>>>>>> 27385ac875470495fb852184f052e5004972b2d0
         }
 
 # This is a url where the most recent plugin package can be downloaded.
